f9f9767ee3eecbc1009a4c0d1d2c73e36dfe89c9
quickdials <quickdials1@gmail.com>
2025-10-09 12:38:01 +0530

    env

--- config/app.php
+++ config/app.php
@@ -51,7 +51,6 @@ return [
     | the application so that it's available within Artisan commands.
     |
     */
-
     'url' => env('APP_URL', 'https://www.quickdials.com/'),
     'website'=>'https://www.quickdials.com/',
     'backend_url'=>'https://api.quickdials.com/',

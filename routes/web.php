639309a23ade598bd8420d1af01ce0fa02617b75
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-02-19 14:58:14 +0530

    web

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,14 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
- 
+use Illuminate\Support\Facades\Redis;
+Route::get('/redis-test', function () {
+    Redis::set('test_key', 'QuickDials');
+    return Redis::get('test_key');
+});
+  
+
+
 require __DIR__.'/site.php'; 
 
 Route::get('/test', function () {

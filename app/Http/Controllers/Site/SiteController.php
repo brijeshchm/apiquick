4b71ff99eb5cf30256390a0e167d3f33786c0162
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-03-15 19:53:03 +0530

    empty zone

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -149,6 +149,9 @@ class SiteController extends Controller
 
 		$zones = DB::table('citylists')->join('zones', 'zones.city_id', '=', 'citylists.id')->where('citylists.city', 'LIKE', $city)->select('zones.id', 'zones.zone')->orderBy('zones.zone','asc')->distinct()->get();
 		 
+		 if(!$zones->count()){
+				$zones =[ ['id'=>'5','zone'=>'bangalore']];
+		 }
 
 		// dd($zones);
 		$data['keyword'] = array(

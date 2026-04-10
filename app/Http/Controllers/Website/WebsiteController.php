dc44e6d76d4fe0b8420de9dc26e7e8bf8f0320e5
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-04-10 20:32:21 +0530

    city keyw

--- app/Http/Controllers/Website/WebsiteController.php
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -94,12 +94,16 @@ class WebsiteController extends Controller
 		$search_kw = $request->input('keyword');
 		$city = $request->input('city');
 		$cityName = ucwords(str_replace('-', ' ', $city));
-		$keywordName = ucwords(str_replace('-', ' ', $search_kw));
+		//$keywordName = ucwords(str_replace('-', ' ', $search_kw));
+		
+		$city = strtolower(str_replace(' ', '-', trim($request->input('city'))));
+		$search_kw = strtolower(str_replace(' ', '-', trim($request->input('keyword'))));
+ 
 
 		$keywordDetails = DB::table('keyword')
 			->join('parent_category', 'keyword.parent_category_id', '=', 'parent_category.id')
 			->join('child_category', 'keyword.child_category_id', '=', 'child_category.id')
-			->where('keyword', 'LIKE', '' . ucwords(str_replace('-', ' ', $search_kw)) . '')
+			->where('keyword.slug', $search_kw)
 			->select('keyword.*', 'parent_category.*', 'child_category.*', 'keyword.id as key_id', 'keyword.faqq1', 'keyword.faqa1', 'keyword.faqq2', 'keyword.faqa2', 'keyword.faqq3', 'keyword.faqa3', 'keyword.faqq4', 'keyword.faqa4', 'keyword.faqq5', 'keyword.faqa5', 'keyword.meta_title', 'keyword.meta_description', 'keyword.meta_keywords', 'keyword.top_description', 'keyword.bottom_description', 'keyword.ratingvalue', 'keyword.ratingcount')
 			->first();
 		if (!$keywordDetails) {
@@ -183,8 +187,8 @@ class WebsiteController extends Controller
 		);
 
 
-
-
+ 
+ 
 		$clientscheck = DB::table('clients')
 			->join('assigned_kwds', 'clients.id', '=', 'assigned_kwds.client_id')
 			->join('keyword', 'assigned_kwds.kw_id', '=', 'keyword.id')
@@ -205,9 +209,9 @@ class WebsiteController extends Controller
 				'c.rating',
 				'c.comment_count'
 			)
-			->where('citylists.city', 'LIKE', "{$cityName}")
+			->where('citylists.city',$city)
 			// ->where('clients.active_status', '1')
-			->where('keyword.keyword', 'LIKE', "{$keywordName}")
+			->where('keyword.slug', $search_kw)
 			// ->groupBy('clients.id')
 			->distinct('clients.id')
 			->orderByRaw("
@@ -226,6 +230,10 @@ class WebsiteController extends Controller
 			$clientsList = $clientscheck;
 		} else {
 		 
+		 
+		 
+			
+	 
 			$clientsList = DB::table('clients')
 				->join('assigned_kwds', 'clients.id', '=', 'assigned_kwds.client_id')
 				->join('assigned_zones', 'clients.id', '=', 'assigned_zones.client_id')
@@ -240,16 +248,16 @@ class WebsiteController extends Controller
 					'clients.*',
 					'clients.id as business_id',
 						 'assigned_kwds.*',
-				'citylists.city',
-					 'keyword.keyword as keyword',
-					 'keyword.slug as slug',
+					'citylists.city',
+					 'keyword.keyword as keywords',
+					 'keyword.slug as slugs',
 					'clients.client_type',
 					'c.rating',
 					'c.comment_count'
 				)
-				->where('keyword.keyword', 'LIKE', '' . $keywordName . '')
-				// ->groupBy('clients.id')
-				->distinct('clients.id')
+				->where('keyword.slug', $search_kw)
+				 ->groupBy('clients.id')
+				 
 				->orderByRaw("
             CASE clients.client_type
                 WHEN 'platinum' THEN 1
@@ -260,8 +268,7 @@ class WebsiteController extends Controller
             END
         ")
 				->get();
-	 
-
+ 
 		}
 
 
@@ -2030,6 +2037,7 @@ class WebsiteController extends Controller
 			}
 
 			$blogPageList[$key] = [
+				'id' => $blog->id,
 				'name' => $blog->name,
 				'url' => 'blog/' . $blog->slug,
 				'img' => $image,
@@ -2128,6 +2136,7 @@ class WebsiteController extends Controller
 				$blogPageList[$key] = [
 					'name' => $blog->name,
 					'url' => 'blog/' . $blog->slug,
+					'id' => $blog->id,
 					'img' => $image,
 					'alt' => $alt,
 					'title' => $blog->name,
@@ -2139,8 +2148,7 @@ class WebsiteController extends Controller
 			}
 		}
 
-		$data['blogList'] = $blogPageList;
-		 
+		$data['blogList'] = $blogPageList; 
 		
 		$blogdetails = Blogdetails::where('blogdetails.status', '1')
             ->where('blogdetails.slug', $slug)
@@ -2175,6 +2183,7 @@ class WebsiteController extends Controller
 
 
 			$blogPageDetails = [
+				'id' => $blogdetails->id,
 				'name' => $blogdetails->name,
 				'url' => 'blog/' . $blogdetails->slug,
 				'blogImage' => $blogimage,

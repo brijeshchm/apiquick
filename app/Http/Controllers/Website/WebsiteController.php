70114f3404910e328bcf6b7d313ddf170966dbad
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-05-29 18:30:08 +0530

    in citu

--- app/Http/Controllers/Website/WebsiteController.php
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -2725,23 +2725,23 @@ class WebsiteController extends Controller
 		}
 
 		if (!empty($keywordDetails->meta_title)) {
-			$meta_title = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->meta_title);
+			$meta_title = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->meta_title);
 		} else {
-			$meta_title = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->keyword);
+			$meta_title = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->keyword);
 
 		}
 		if (!empty($keywordDetails->meta_keywords)) {
-			$meta_keywords = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->meta_keywords);
+			$meta_keywords = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->meta_keywords);
 
 
 		} else {
-			$meta_keywords = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->keyword);
+			$meta_keywords = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->keyword);
 
 		}
 
 
 		if (!empty($keywordDetails->meta_description)) {
-			$meta_description = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->meta_description);
+			$meta_description = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->meta_description);
 
 
 		} else {
@@ -2751,11 +2751,11 @@ class WebsiteController extends Controller
 
 		$top_description = "";
 		if (!empty($keywordDetails->top_description)) {
-			$top_description = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->top_description);
+			$top_description = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->top_description);
 		}
 		$bottom_description = "";
 		if (!empty($keywordDetails->bottom_description)) {
-			$bottom_description = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->bottom_description);
+			$bottom_description = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->bottom_description);
 		}
 		$courseabout ="";
 		$heading ="";
@@ -2766,28 +2766,28 @@ class WebsiteController extends Controller
 		$paragraph5 ="";
 		$paragraph6 ="";
 		if (!empty($keywordDetails->courseabout)) {
-			$courseabout = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->courseabout);
+			$courseabout = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->courseabout);
 		}
 		if (!empty($keywordDetails->heading)) {
-			$heading = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->heading);
+			$heading = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->heading);
 		}
 		if (!empty($keywordDetails->paragraph1)) {
-			$paragraph1 = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->paragraph1);
+			$paragraph1 = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->paragraph1);
 		}
 		if (!empty($keywordDetails->paragraph2)) {
-			$paragraph2 = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->paragraph2);
+			$paragraph2 = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->paragraph2);
 		}
 		if (!empty($keywordDetails->paragraph3)) {
-			$paragraph3 = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->paragraph3);
+			$paragraph3 = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->paragraph3);
 		}
 		if (!empty($keywordDetails->paragraph4)) {
-			$paragraph4 = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->paragraph4);
+			$paragraph4 = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->paragraph4);
 		}
 		if (!empty($keywordDetails->paragraph5)) {
-			$paragraph5 = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->paragraph5);
+			$paragraph5 = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->paragraph5);
 		}
 		if (!empty($keywordDetails->paragraph6)) {
-			$paragraph6 = preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->paragraph6);
+			$paragraph6 = preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->paragraph6);
 		}
 
 	 
@@ -2811,27 +2811,27 @@ class WebsiteController extends Controller
 			'top_description' => $top_description,
 			'bottom_description' => $bottom_description,
 		 
-			'bottom_heading' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->bottom_heading),
-			'top_heading' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->top_heading),
-			'extra_heading' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->extra_heading),
-			'extra_description' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->extra_description),
+			'bottom_heading' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->bottom_heading),
+			'top_heading' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->top_heading),
+			'extra_heading' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->extra_heading),
+			'extra_description' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->extra_description),
 		 
 			
 			
 			
 			
-			'faqa1' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqa1),
-			'faqq2' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqq2),
-			'faqa2' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqa2),
-			'faqq3' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqq3),
-			'faqa3' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqa3),
-			'faqq4' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqq4),
-			'faqa4' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqa4),
-			'faqq5' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqq5),
-			'faqa5' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqa5),
+			'faqa1' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqa1),
+			'faqq2' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqq2),
+			'faqa2' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqa2),
+			'faqq3' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqq3),
+			'faqa3' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqa3),
+			'faqq4' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqq4),
+			'faqa4' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqa4),
+			'faqq5' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqq5),
+			'faqa5' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqa5),
 			
-			'faqq6' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqq6),
-			'faqa6' => preg_replace('/{{city}}/i', ucfirst($city), $keywordDetails->faqa6),
+			'faqq6' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqq6),
+			'faqa6' => preg_replace('/in {{city}}/i', ucfirst($city), $keywordDetails->faqa6),
 			
 			'courseabout' => $courseabout,
 			'heading' => $heading,
